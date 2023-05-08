87be3fdecd106bbfdd50e7b31567f1b12800de9e
Robert Baes <you@example.com>
2023-05-08 10:17:42 +0100

    final

--- GameStore-app/resources/views/games/show.blade.php
+++ GameStore-app/resources/views/games/show.blade.php
@@ -3,12 +3,15 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2> Show Game</h2>
-            </div>
+            {{-- <div class="pull-left">
+                <h2>Game</h2>
+            </div> --}}
+            <br>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('games.index') }}"> Back</a>
             </div>
+
+            <br>
         </div>
     </div>
 
@@ -16,12 +19,13 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Name:</strong>
-                {{ $game->name }}
+               <h2> {{ $game->name }} </h2>
             </div>
+            <br>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Details:</strong>
+                {{-- <strong>Details:</strong> --}}
                 {{ $game->detail }}
             </div>
         </div>
